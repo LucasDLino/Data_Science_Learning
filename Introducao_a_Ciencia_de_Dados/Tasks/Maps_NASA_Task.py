a2ade8375df3cc01cc2eb9b0fe28edbc340f7b10
Lucas Lino <lucasdiego513@hotmail.com>
2020-02-04 11:41:08 -0200

    Implemented Maps and NASA Task.

---
+++ Introducao_a_Ciencia_de_Dados/Tasks/Maps_NASA_Task.py
@@ -0,0 +1,61 @@
+from netCDF4 import Dataset, num2date
+from pathlib import Path
+import pandas as pd
+import datetime as dt
+import numpy as np
+from mpl_toolkits.basemap import Basemap, addcyclic, shiftgrid
+import matplotlib.pyplot as plt
+
+'''
+To run basemap you need to install PROJ4 and GEOS(put it in the path).
+
+Todos os demais pacotes devem ser instalados/atualizados utilizando o comando 'conda install/update'
+
+netCDF4 deve ser instalado utilizando pip install
+
+Deve-se especificar a variável de ambiente PROJ_LIB
+
+Basemap foi baixado (.whl) e depois instalado via linha de comando
+'''
+
+nasa_path = Path(r"C:\Users\lucas\PycharmProjects\Files")
+
+nc = Dataset(nasa_path.joinpath("MERRA2_400.tavg1_2d_slv_Nx.20190801.nc4"), mode='r')
+
+## All variables
+for i in nc.variables:
+    print (i, nc.variables[i].shape)
+
+variable = "T250"
+
+#Extracting data from netCDF file
+lats = nc.variables['lat'][:]
+lons = nc.variables['lon'][:]
+data = nc.variables[variable][:]
+time = num2date(nc.variables['time'][:], nc.variables['time'].units)
+
+# Plotting
+fig = plt.figure(figsize=(16,9))
+fig.subplots_adjust(left=0, right=1, bottom=0, top=0.9)
+
+m = Basemap(projection='moll', llcrnrlat=-90, urcrnrlat=90,
+            llcrnrlon=0, urcrnrlon=360, resolution='c', lon_0=0)
+
+m.drawcoastlines()
+m.drawmapboundary()
+
+# Make the plot continuous
+data_cyclic, lons_cyclic = addcyclic(data[0, :, :], lons) #00:30H
+# Shift the grid so lons go from -180 to 180 instead of 0 to 360.
+data_cyclic, lons_cyclic = shiftgrid(180., data_cyclic, lons_cyclic, start=False)
+# Create 2D lat/lon arrays for Basemap
+lon2d, lat2d = np.meshgrid(lons_cyclic, lats)
+# Transforms lat/lon into plotting coordinates for projection
+x, y = m(lon2d, lat2d)
+# Plot of air temperature with 11 contour intervals
+cs = m.contourf(x, y, data_cyclic, 11, cmap=plt.set_cmap('Spectral_r'))
+cbar = plt.colorbar(cs, orientation='horizontal', shrink=0.5)
+cbar.set_label("%s (%s)" % (nc.variables[variable].standard_name,
+                            nc.variables[variable].units))
+plt.title("%s on %s" % (nc.variables[variable].standard_name, time[0]))
+plt.show()
